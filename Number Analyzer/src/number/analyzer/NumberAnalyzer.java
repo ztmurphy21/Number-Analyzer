7925a9ac548cf75849596f5cd1543beec87b74ea
Zachary Murphy <murphyz@my.wccc.edu>
2016-03-29 21:29:23 -0400

    almost there.

--- Number Analyzer/src/number/analyzer/NumberAnalyzer.java
+++ Number Analyzer/src/number/analyzer/NumberAnalyzer.java
@@ -5,34 +5,106 @@
  */
 package number.analyzer;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Scanner;
+
 /**
  *
  * @author Zachary Murphy
  * @param <T>
  */
 public class NumberAnalyzer< T extends Number>{
-    private T[] array;
-    public NumberAnalyzer(T[] arr){
-        array = arr;
+   Scanner keyboard = new Scanner(System.in);
+    private final  T[] array;
+    private boolean order = false;
+    private Double sumOfNumbers;
+    
+    /*
+    constructor for Number 
+    */
+    public NumberAnalyzer(T[] array){
+        this.array = array;
     }
     
-    public T getLow(){
-        T low = array[0];
-        for (int x =1; x < array.length; x++){
-            if(array[x].intValue()<0){
-                low = array[x];
-            } else {
-            }
-        }
-                    return low;
+/*
+    will calculate the sume in the array
+    */
+    
+  public Double sumOfNumbers(){
+      this.calcSum();
+      return this.sumOfNumbers;
+  }
+  
 
-    }
+  /*
+   will calculate the sum in array
+   */
+  private void calcSum(){
+      if (sumOfNumbers == null){
+              Collection<? extends Number> numsCollection = Arrays.asList(this.array);
+              this.sumOfNumbers = numsCollection
+                                 .stream()
+            .mapToDouble(Number::doubleValue)
+            .reduce(0.0, (a,b) -> a + b);
+             
+      }
+  }
+  
+  /*
+  will get average number in array
+  */
+  public Double Avg(){
+    this.calcSum();
+    double avg = this.sumOfNumbers/this.array.length;
+    return avg;
+}
+  /*
+  will get highest number in array
+  */
+  
+  public T getHighest(){
+          // Assume the first element contains the highest
+      // value in the array.
+      T highest = array[0];
+      
+      // Step through the rest of the array looking
+      // for higher values, using a for loop
+      for (int index = 1; index < array.length; index++)
+      {
+         if (array[index].doubleValue() > highest.doubleValue())
+            highest = array[index];
+  }
+        return highest;
+  }
+  /*
+  will get lowest number in array
+  */
+  public T getLowest(){
+      T lowest = array[0];
+        // Step through the rest of the array looking
+      // for lower values, using a for loop
+      for (int index = 1; index < array.length; index++)
+      {
+         if (array[index].doubleValue() < lowest.doubleValue())
+            lowest = array[index];
+  }
+        return lowest;
+      
+  }
+  // get total number of elements in array
+  private Double elements(Double x, Double y){
+      return x+y;
+  }
+      
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double [] numberLists = {1,2,3};
+         NumberAnalyzer<double> n1 = new  NumberAnalyzer<double>(numberLists);
+         System.out.println();
     }
     
 }
